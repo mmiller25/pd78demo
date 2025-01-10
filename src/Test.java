7f82ba34cd3872764667009c5bb2499a5489e7f3
BT_1N3_34 <sample@bths.edu>
2025-01-10 13:09:48 -0500

    dogs <3

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("but dogs are better");
         System.out.println("friday!");
         System.out.println("goodbye");
         System.out.println("TGIF!");
